1878ce92b58122b722a17dd47468083f3e08395c
Nipuna Rambukkanage <nipunaram@gmail.com>
2024-05-06 20:12:32 +0530

    added connection string

--- config/db.js
+++ config/db.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 async function connectDB(){
     try{
-        const password = encodeURIComponent('replace_this_with_your_password');
-        await mongoose.connect(`mongodb+srv://nipunaram:${password}@cluster0.5xeifld.mongodb.net/?retryWrites=true&w=majority`, 
+        //const password = encodeURIComponent('replace_this_with_your_password');
+        //await mongoose.connect(`mongodb+srv://nipunaram:${password}@cluster0.5xeifld.mongodb.net/?retryWrites=true&w=majority`, 
+        await mongoose.connect('mongodb+srv://nipunaram:aP7eOUGBDrBA5BXB@cluster0.5xeifld.mongodb.net/?retryWrites=true&w=majority',
         {
             useNewUrlParser : true,
             useUnifiedTopology : true
